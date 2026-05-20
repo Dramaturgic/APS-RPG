23efe2fc0d6671cdd698f4b0fd2d8bf5e7c5c697
Igor Calizaya <128617484+Dramaturgic@users.noreply.github.com>
2026-05-20 16:32:11 -0300

    Delete src directory

--- src/Esqueleto.java
+++
@@ -1,9 +0,0 @@
-public class Esqueleto extends Inimigo {
-
-    public Esqueleto(String nome, int pontosDeVida, int ataque, int defesa) {
-        super(nome, pontosDeVida, ataque, defesa);
-    }
-}
-
-
-// WIP
